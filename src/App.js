b0cd1c7174b43227fb71076cfff6a1439a2c84a2
SuvorovNikita <nikit.suvorov@gmail.com>
2022-01-12 17:46:24 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 class="vacancy">Начинающий web-программист / Junior Frontend Developer</h2>
+      <h3 class="name">Никита Суворов</h3>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let vacancy = document.querySelector('.vacancy');
+let name = document.querySelector('.name');
+
+vacancy.addEventListener('click', function () {
+  vacancy.hidden = true;
+});
+
+name.addEventListener('click', function () {
+  name.hidden = true;
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.code === 'Space') {
+    name.remove();
+    vacancy.remove();
+  }
+});
